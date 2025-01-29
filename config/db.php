afde4eab8883192e9e5e89e88f40d1f2302aea02
Swaraj Yadav <swarajy26@gmail.com>
2025-01-28 19:36:33 -0500

    Added oracle stuff to db php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 // config/db.php
 
-$host = "sql110.infinityfree.com";
-$dbName = "if0_38191118_invoicing_db";
-$username = "if0_38191118";
-$password = "Swaraj2601"; // or your MySQL password if you've set one
+$host = "localhost";
+$dbName = "invoicing_db";
+$username = "invoice_user";
+$password = "StrongPassword123"; // or your MySQL password if you've set one
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $username, $password);
